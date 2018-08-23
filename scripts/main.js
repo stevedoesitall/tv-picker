70ad8241afff59abb2e386691befafacee422c92
Steve Giordano <stephenagiordano@gmail.com>
2018-08-22 23:11:10 -0400

    Pushing live

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,28 @@
-// import { get_id, headers, cl, date, string } from "./ditko.js";
+import { get_id, headers, cl, string, create_el } from "https://rawgit.com/stevedoesitall/ditkojs/master/ditko.js";
+
+const submit_btn = get_id("submit");
+
+submit_btn.addEventListener("click", function submit_form() {
+    const show = get_id("shows").value;
+    // alert(`Getting recs for ${show}`);
+    fetch("/server", {
+        method: "post",
+        headers: headers,
+        body: string({id: show})
+    })
+    .then(
+        function(response) {
+        if (response.status != 200) {
+            cl("Error: " + response.status);
+            return;
+        }
+        response.json().then(
+            function(resp_data) {
+                cl(resp_data);
+                get_id("recs_ctnr").innerHTML = "";
+
+                get_id("recs_ctnr").innerHTML += "<p>" + resp_data.name + " - " + "Season " + resp_data.season_number + ", Episode " + resp_data.episode_number + "</p><p>" + resp_data.overview + "</p>";
+            })
+        })
+        .catch(error => cl(error) );
+});
--- scripts/server.js
+++ scripts/server.js
@@ -3,20 +3,35 @@ const port = process.env.PORT || 3000;
 const express = require("express");
 const body_parser = require("body-parser");
 const path = require("path");
-const http = require("http");
+const https = require("https");
 const app = express();
-const server = http.createServer(app);
+const server = https.createServer(app);
 const dir = path.join(__dirname, "../");
+const uri = "theoffice";
+const encode = encodeURI(uri);
+
+const api_key = "96b4c29a230ca6080bb875ea7f415745";
+//Example request: https://api.themoviedb.org/3/movie/550?api_key=96b4c29a230ca6080bb875ea7f415745
+
 
 app.use(express.static(dir));
 app.use(body_parser.urlencoded({ extended: false }));
 app.use(body_parser.json());
 app.listen(port, () => console.log("TV Picker started on port " + port));
 
-app.post("/email", function(req, res) {
-    http.get('http://nodejs.org/dist/index.json', (res) => {
-    const { statusCode } = res;
-    const contentType = res.headers['content-type'];
+app.post("/server", function(req, res) {
+    const id = req.body.id;
+    const show_options = {
+        "method": "GET",
+        "hostname": "api.themoviedb.org",
+        "port": null,
+        "path": "/3/tv/" + id + "?api_key=" + api_key,
+        "headers": {}
+      };
+
+    https.get(show_options, (cb) => {
+    const { statusCode } = cb;
+    const contentType = cb.headers['content-type'];
 
     let error;
     if (statusCode !== 200) {
@@ -29,17 +44,110 @@ app.post("/email", function(req, res) {
     if (error) {
         console.error(error.message);
         // consume response data to free up memory
-        res.resume();
+        cb.resume();
         return;
     }
 
-    res.setEncoding('utf8');
+    cb.setEncoding('utf8');
     let rawData = '';
-    res.on('data', (chunk) => { rawData += chunk; });
-    res.on('end', () => {
+    cb.on('data', (chunk) => { rawData += chunk; });
+    cb.on('end', () => {
         try {
         const parsedData = JSON.parse(rawData);
-        console.log(parsedData);
+        console.log('data', parsedData.seasons.length);
+        const season_num = parsedData.seasons.length;
+        // res.send(parsedData);
+            //Get season data
+            const rand_season = Math.floor(Math.random() * (season_num - 1 + 1)) + 1;
+            const season_options = {
+                "method": "GET",
+                "hostname": "api.themoviedb.org",
+                "port": null,
+                "path": "/3/tv/" + id + "/season/" + rand_season + "?api_key=" + api_key,
+                "headers": {}
+              };
+        
+            https.get(season_options, (cb) => {
+            const { statusCode } = cb;
+            const contentType = cb.headers['content-type'];
+        
+            let error;
+            if (statusCode !== 200) {
+                error = new Error('Request Failed.\n' +
+                                `Status Code: ${statusCode}`);
+            } else if (!/^application\/json/.test(contentType)) {
+                error = new Error('Invalid content-type.\n' +
+                                `Expected application/json but received ${contentType}`);
+            }
+            if (error) {
+                console.error(error.message);
+                // consume response data to free up memory
+                cb.resume();
+                return;
+            }
+        
+            cb.setEncoding('utf8');
+            let rawData = '';
+            cb.on('data', (chunk) => { rawData += chunk; });
+            cb.on('end', () => {
+                try {
+                const parsedData = JSON.parse(rawData);
+                    //Get episode data
+                    const episode_num = parsedData.episodes.length;
+                    const rand_episode = Math.floor(Math.random() * (episode_num - 1 + 1)) + 1;
+                    const episode_options = {
+                        "method": "GET",
+                        "hostname": "api.themoviedb.org",
+                        "port": null,
+                        "path": "/3/tv/" + id + "/season/" + rand_season + "/episode/" + rand_episode + "?api_key=" + api_key,
+                        "headers": {}
+                      };
+                
+                    https.get(episode_options, (cb) => {
+                    const { statusCode } = cb;
+                    const contentType = cb.headers['content-type'];
+                
+                    let error;
+                    if (statusCode !== 200) {
+                        error = new Error('Request Failed.\n' +
+                                        `Status Code: ${statusCode}`);
+                    } else if (!/^application\/json/.test(contentType)) {
+                        error = new Error('Invalid content-type.\n' +
+                                        `Expected application/json but received ${contentType}`);
+                    }
+                    if (error) {
+                        console.error(error.message);
+                        // consume response data to free up memory
+                        cb.resume();
+                        return;
+                    }
+                
+                    cb.setEncoding('utf8');
+                    let rawData = '';
+                    cb.on('data', (chunk) => { rawData += chunk; });
+                    cb.on('end', () => {
+                        try {
+                        const parsedData = JSON.parse(rawData);
+                        res.send(parsedData);
+                            //Get episode data
+                            
+                
+                        } catch (e) {
+                        console.error(e.message);
+                        }
+                    });
+                    }).on('error', (e) => {
+                    console.error(`Got error: ${e.message}`);
+                    });
+        
+                } catch (e) {
+                console.error(e.message);
+                }
+            });
+            }).on('error', (e) => {
+            console.error(`Got error: ${e.message}`);
+            });
+
         } catch (e) {
         console.error(e.message);
         }
