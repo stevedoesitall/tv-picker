c3ebba5f7ab51ca2ff732db2a475a98f4e746096
stevedoesitall <stevegiordano@MBA0395.local>
2019-03-13 18:40:32 -0400

    CSS updates

--- scripts/main.js
+++ scripts/main.js
@@ -9,12 +9,12 @@ season_check.addEventListener("click", function show_seasons() {
     if (season_check.checked == true) {
         season_selection.style.display = "block";
         const show = get_id("shows").value;
-        const data = "seasons";
-        // alert(`Getting recs for ${show}`);
+        const fetch_type = "seasons";
+        console.log(fetch_type);
         fetch("/server", {
             method: "post",
             headers: headers,
-            body: string({id: show, data: data})
+            body: string({id: show})
         })
         .then(
             function(response) {
